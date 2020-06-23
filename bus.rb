c9804d5d60f9615f9bdc0197062dd7bc88f8ed08
James Monks <J.monks23@gmail.com>
2020-06-23 14:52:40 +0100

    drive and total passengers test pass

---
+++ bus.rb
@@ -0,0 +1,19 @@
+class Bus
+    attr_reader :route_number, :destination
+    attr_accessor :passengers
+
+    def initialize(route_number, destination)
+        @route_number = route_number
+        @destination = destination
+        @passengers = []
+    end
+
+    def drive()
+        return "Brum brum"
+    end
+
+    def total_passengers()
+        return @passengers.length()
+    end
+
+end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -2,8 +2,21 @@ require('minitest/autorun')
 require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+require_relative("../bus")
 require_relative('../person')
 
 class BusTest < MiniTest::Test
+
+    def setup()
+        @bus = Bus.new(22, "Ocean Terminal")
+    end
+
+    def test_drive()
+        assert_equal("Brum brum", @bus.drive())
+    end
+
+    def test_total_passengers()
+        assert_equal(0, @bus.total_passengers())
+    end
 
 end
